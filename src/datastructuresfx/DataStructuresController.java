ee4f372ae627bf9751fa2321cc9ce7ab2a0cd00e
theBashShell <edohbruno@gmail.com>
2017-08-11 09:47:29 +0000

    Updated Master

---
+++ src/datastructuresfx/DataStructuresController.java
@@ -0,0 +1,18 @@
+package datastructuresfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author bruno
+ */
+public class DataStructuresController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
---
+++ src/datastructuresfx/DataStructuresFX.java
@@ -0,0 +1,32 @@
+package datastructuresfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author bruno
+ */
+public class DataStructuresFX extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("DataStructures.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
