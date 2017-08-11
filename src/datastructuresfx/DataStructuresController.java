908077f1289e320af4e3e1f10ae6c0bb2c76e0a5
theBashShell <edohbruno@gmail.com>
2017-08-11 10:58:14 +0000

    Added character symbols to all Stack buttons

--- src/datastructuresfx/DataStructuresController.java
+++ src/datastructuresfx/DataStructuresController.java
@@ -1,7 +1,10 @@
 package datastructuresfx;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -10,9 +13,34 @@ import javafx.fxml.Initializable;
  */
 public class DataStructuresController implements Initializable {
 
+    @FXML
+    private JFXButton spush;
+    @FXML
+    private JFXButton spop;
+    @FXML
+    private JFXButton sempty;
+    @FXML
+    private JFXButton speek;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+
+    @FXML
+    private void stackPush(ActionEvent event) {
+    }
+
+    @FXML
+    private void stackPop(ActionEvent event) {
+    }
+
+    @FXML
+    private void isStackEmpty(ActionEvent event) {
+    }
+
+    @FXML
+    private void stackPeek(ActionEvent event) {
     }
 
 }
