c7e6657ed672587c97bfe2e533f39ccb62110350
theBashShell <edohbruno@gmail.com>
2017-08-11 12:00:41 +0000

    Done implementing Stack pop method

--- src/datastructuresfx/DataStructuresController.java
+++ src/datastructuresfx/DataStructuresController.java
@@ -1,17 +1,22 @@
 package datastructuresfx;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.TilePane;
 
 /**
  *
  * @author bruno
  */
 public class DataStructuresController implements Initializable {
+
+    private static int size = 0;
 
     @FXML
     private JFXButton spush;
@@ -21,6 +26,10 @@ public class DataStructuresController implements Initializable {
     private JFXButton sempty;
     @FXML
     private JFXButton speek;
+    @FXML
+    private TilePane stackDS;
+    @FXML
+    private JFXTextField sdata;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -29,10 +38,24 @@ public class DataStructuresController implements Initializable {
 
     @FXML
     private void stackPush(ActionEvent event) {
+        String data = sdata.getText();
+        if (data.length() < 1) {
+            return;
+        }
+        size++;
+        Button button = new Button(data);
+        button.setMinWidth(stackDS.widthProperty().doubleValue());
+        button.setStyle("-fx-background-color: wheat;");
+        stackDS.getChildren().add(0, button);
     }
 
     @FXML
     private void stackPop(ActionEvent event) {
+        if (size == 0) {
+            return;
+        }
+        stackDS.getChildren().remove(0);
+        size--;
     }
 
     @FXML
